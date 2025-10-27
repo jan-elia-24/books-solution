81585560e07db1fbd594def1648e020b4d8da798
Jan Elia <jan.elia995@hotmail.com>
2025-10-27 03:55:43 +0100

    add AuthGuard and protect books routes

--- books-app/src/app/app.routes.ts
+++ books-app/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { LoginComponent } from './pages/login/login';
 import { BooksListComponent } from './pages/books-list/books-list';
 import { BookFormComponent } from './pages/book-form/book-form';
 import { QuotesComponent } from './pages/quotes/quotes';
+import { authGuard } from './core/auth-guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'books', component: BooksListComponent },
-  { path: 'books/new', component: BookFormComponent },
-  { path: 'books/:id/edit', component: BookFormComponent },
+  { path: 'books', component: BooksListComponent, canActivate: [authGuard] },
+  { path: 'books/new', component: BookFormComponent, canActivate: [authGuard] },
+  { path: 'books/:id/edit', component: BookFormComponent, canActivate: [authGuard] },
   { path: 'quotes', component: QuotesComponent },
   { path: '', pathMatch: 'full', redirectTo: 'books' },
   { path: '**', redirectTo: 'books' },
