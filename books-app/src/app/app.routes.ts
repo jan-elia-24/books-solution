8914ca25735f272992ea6544a9fe841864b4f6e6
Jan Elia <jan.elia995@hotmail.com>
2025-10-27 01:49:38 +0100

    add empty pages (login, books-list, book-form, quotes) and wire routes

--- books-app/src/app/app.routes.ts
+++ books-app/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login';
+import { BooksListComponent } from './pages/books-list/books-list';
+import { BookFormComponent } from './pages/book-form/book-form';
+import { QuotesComponent } from './pages/quotes/quotes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'books', component: BooksListComponent },
+  { path: 'books/new', component: BookFormComponent },
+  { path: 'books/:id/edit', component: BookFormComponent },
+  { path: 'quotes', component: QuotesComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'books' },
+  { path: '**', redirectTo: 'books' },
+];
